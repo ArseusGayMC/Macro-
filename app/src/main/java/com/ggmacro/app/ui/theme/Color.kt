ce74511accda8ad092556b9171a14e0040cebb64
ArseusGayMC <71520709+ArseusGayMC@users.noreply.github.com>
2026-06-17 00:20:29 +0300

    Add app/src/main/java/com/ggmacro/app/ui/theme/Color.kt

---
+++ app/src/main/java/com/ggmacro/app/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.ggmacro.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GamingBlack = Color(0xFF0A0A0F)
+val GamingDarkSurface = Color(0xFF12121A)
+val GamingCard = Color(0xFF1A1A26)
+val GamingCardElevated = Color(0xFF22223A)
+val GamingBorder = Color(0xFF2D2D4A)
+
+val NeonCyan = Color(0xFF00F5FF)
+val NeonCyanDim = Color(0xFF00C4CC)
+val NeonPurple = Color(0xFF9D4EDD)
+val NeonPurpleDim = Color(0xFF7B3AB4)
+val NeonGreen = Color(0xFF39FF14)
+val NeonRed = Color(0xFFFF3131)
+val NeonOrange = Color(0xFFFF6B00)
+
+val TextPrimary = Color(0xFFEEEEFF)
+val TextSecondary = Color(0xFF9090B0)
+val TextDisabled = Color(0xFF505070)
